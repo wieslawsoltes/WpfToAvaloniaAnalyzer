1a64ef82fa6fd2ba54ca53592f90317416ea129d
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2025-10-28 16:06:14 +0100

    Update RoutedEventSamples.cs

--- WpfToAvaloniaAnalyzers.Sample.Wpf/Samples/RoutedEventSamples.cs
+++ WpfToAvaloniaAnalyzers.Sample.Wpf/Samples/RoutedEventSamples.cs
@@ -5,7 +5,49 @@ namespace WpfToAvaloniaAnalyzers.Sample.Wpf.Samples;
 
 public class RoutedEventSamples : Control
 {
-    public static readonly global::Avalonia.Interactivity.RoutedEvent<global::Avalonia.Interactivity.RoutedEventArgs> FooEvent = global::Avalonia.Interactivity.RoutedEvent.Register<RoutedEventSamples, global::Avalonia.Interactivity.RoutedEventArgs>("Foo", global::Avalonia.Interactivity.RoutingStrategies.Bubble);
+    public static readonly RoutedEvent FooEvent =
+        EventManager.RegisterRoutedEvent(
+            "Foo",
+            RoutingStrategy.Bubble,
+            typeof(RoutedEventHandler),
+            typeof(RoutedEventSamples));
 
-    public static readonly global::Avalonia.Interactivity.RoutedEvent BarEvent = Button.ClickEvent;
+    public static readonly RoutedEvent BarEvent = Button.ClickEvent;
+
+    public static readonly DependencyProperty BazProperty =
+        DependencyProperty.Register(
+            "Baz",
+            typeof(int),
+            typeof(RoutedEventSamples),
+            new PropertyMetadata(0, OnBazChanged));
+
+    public event RoutedEventHandler Foo
+    {
+        add => AddHandler(FooEvent, value);
+        remove => RemoveHandler(FooEvent, value);
+    }
+
+    public int Baz
+    {
+        get => (int)GetValue(BazProperty);
+        set => SetValue(BazProperty, value);
+    }
+
+    public RoutedEventSamples()
+    {
+        AddHandler(BarEvent, OnBarRaised, handledEventsToo: true);
+    }
+
+    private static void OnBazChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+    {
+        if (sender is RoutedEventSamples samples)
+        {
+            samples.RaiseEvent(new RoutedEventArgs(FooEvent, samples));
+        }
+    }
+
+    private void OnBarRaised(object sender, RoutedEventArgs e)
+    {
+        RemoveHandler(BarEvent, OnBarRaised);
+    }
 }
